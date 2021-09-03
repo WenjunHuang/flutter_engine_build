e2315793525e815586b51bd83f2bd55f4fc9b392
WenjunHuang <wenjun.huang80@gmail.com>
2021-09-03 09:18:57 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Windows 10下编译flutter engine
+# Windows 10下编译和hack flutter engine
 # 准备工作
 1. 设置好你的代理，以下假设为http://127.0.0.1:9798
 2. 打开powershell
@@ -8,12 +8,12 @@ $env:HTTPS_PROXY="http://127.0.0.1:9798"
 
 # flutter engine编译
 1. git clone --recurse-submodules https://github.com/wenjunhuang/FlutterEngineBuild.git
-1. 修改depot_tools_proxy.patch文件
+2. 修改depot_tools_proxy.patch文件
 将```+  $proxy.Address = "http://127.0.0.1:9798"```里面的地址改为你的代理地址，然后保存
-1. cd depot_tools 并且执行 git apply ..\depot_tools_proxy.patch
-1. 执行fetch-engine.py
-1. 执行build.py
-1. 使用管理员权限打开控制台cd "你的目录"\engine\src\out 执行mklink /J host_debug_unopt .\win_debug_unopt\
-1. 使用visual studio 2019打开\win_debug_unopt\all.sln
-1. 开发、更改代码后，执行python rebuild.py
-1. 重复上面两步骤
+3. cd depot_tools 并且执行 git apply ..\depot_tools_proxy.patch
+4. 执行fetch-engine.py
+5. 执行build.py
+6. 使用管理员权限打开控制台cd "你的目录"\engine\src\out 执行mklink /J host_debug_unopt .\win_debug_unopt\
+7. 使用visual studio 2019打开\win_debug_unopt\all.sln或者使用Clion打开本目录
+8. 执行python watcher.py
+9. 修改engine的代码，会自动编译并运行单元测试（在watcher.py脚本里面设置）
---
+++ watcher.py
@@ -0,0 +1,74 @@
+import os
+import subprocess
+
+import time
+from rx import operators as ops
+from rx import subject as s
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
+
+if __name__ == "__main__":
+    build_subject = s.Subject()
+    build_subject.pipe(
+        ops.debounce(0.2)
+    ).subscribe(lambda value: rebuild())
+
+
+    def on_created(event):
+        print(f"{event.src_path} has been created!")
+        build_subject.on_next(1)
+
+
+    def on_deleted(event):
+        print(f"Someone deleted {event.src_path}!")
+        build_subject.on_next(1)
+
+
+    def on_modified(event):
+        print(f"{event.src_path} has been modified")
+        build_subject.on_next(1)
+
+
+    def rebuild():
+        # visual studio 2019安装路径
+        vs_path = r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
+        # windows kits 安装路径
+        winsdk_path = r"C:\Program Files (x86)\Windows Kits\10"
+        depot_tools_path = os.path.join(os.getcwd(), "depot_tools")
+        env = os.environ.copy()
+        env["DEPOT_TOOLS_WIN_TOOLCHAIN"] = "0"
+        env["GYP_MSVS_OVERRIDE_PATH"] = vs_path
+        env["WINDOWSSDKDIR"] = winsdk_path
+        env["HTTP_PROXY"] = "http://127.0.0.1:9798"  # 设置代理
+        env["HTTPS_PROXY"] = "http://127.0.0.1:9798"  # 设置代理
+        env["PATH"] = "%s;" % depot_tools_path + env["PATH"]
+
+        engine_path = os.path.join(os.getcwd(), "engine")
+        depot_tools_path = os.path.join(os.getcwd(), "depot_tools")
+        subprocess.run([os.path.join(depot_tools_path, "ninja.exe"), "-C",
+                        os.path.join(engine_path, "src", "out", "host_debug_unopt")], env=env, shell=True, check=True)
+
+        unittest_path = os.path.join(engine_path, "src", "out", "host_debug_unopt")
+        subprocess.run([os.path.join(unittest_path, "tonic_unittests.exe"),
+                        "--gtest_filter=-*TimeSensitiveTest*",
+                        "--gtest_repeat=2", "--gtest_shuffle", ],
+                       shell=True,
+                       check=True)
+
+
+    patterns = ["*.c", "*.cc", "*.h", "*.gn", "*.dart"]
+    event_handler = PatternMatchingEventHandler(patterns, None, False, True)
+    event_handler.on_created = on_created
+    event_handler.on_deleted = on_deleted
+    event_handler.on_modified = on_modified
+
+    observer = Observer()
+    engine_path = os.path.join(os.getcwd(), "engine", "src", "flutter")
+    observer.schedule(event_handler, engine_path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
